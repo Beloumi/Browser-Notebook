7718c9b7b8b58967caade681eaca8736da14e931
Axel von dem Bruch <Beloumi@users.noreply.github.com>
2018-02-21 10:49:13 +0100

    Create land.de.js
    version 0.4-beta

---
+++ js/lang/land.de.js
@@ -0,0 +1,31 @@
+// lang.en.js
+"use strict";
+var lang = {
+	//browser-notebook.js
+	 unexpected_error: "Unerwarteter Fehler.",
+	 no_text: "Kein Text vorhanden",
+    unsaved_changes : "Änderungen wurden nicht gespeichert..\n Jetzt speichern?",
+    first_enter_password : "Zuerst muss das Passwort eingegeben werden...",
+    text_lost : "Wenn Du fortfährst, geht der Text verloren...",
+    apply_new_work_factor : "Damit der neue Arbeitsfaktor auf den Text angewendet wird, \nmuss das Passwort geändert werden.",
+	// psw_title_action.js
+	 enter_text_placeholder: "Text eingeben...",
+	 wrong_password: "Falsches Passwort",
+	 existing_title: "Es existiert bereit ein Inhalt mit dem Namen ",
+	 import_overwrites: ".\n Der Import wird den bestehenden Inhalt überschreiben...", 
+	 type_and_confirm: "Passwort eingeben und bestätigen...",
+	 confirm_password: "Passwort bestätigen...",
+	 password_confirm_not_equal: "Passwort und Bestätigung sind nicht gleich",
+	 no_title_error: "Fehler: Es liegt kein Titel vor",
+	// file_action.js
+	 missing_required_json_key: "JSON enthält einen erforderliche Eigenschaft nicht", 
+	 not_valid_json: "Datei ist kein gültiges JSON Format...", 
+	 invalid_notebook_file: "Ungültige Browser-Notebook-Datei: ", 
+	 no_support_download: "Dieser Browser unterstützt keine Download-Funktion",
+	 no_support_upload: "Dieser Browser unterstützt keine Upload-Funktion",
+	// page_view.js
+	 no_text_create: "Es liegt kein Text vor. \nErzeuge neuen Text: \n  Datei -> Neu...",
+	 // pell_integration
+	 select_image: "Bilddatei auswählen"
+};
+
