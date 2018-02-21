531d114002b98577e3dcfe06a714f9f3d7be9dde
Axel von dem Bruch <Beloumi@users.noreply.github.com>
2018-02-21 10:48:29 +0100

    Create lang.en.js
    version 0.4-beta

---
+++ js/lang/lang.en.js
@@ -0,0 +1,31 @@
+// lang.en.js
+"use strict";
+var lang = {
+	//browser-notebook.js
+	 unexpected_error: "Unexpected error.",
+	 no_text: "There is no text",
+    unsaved_changes : "There a unsaved changes..\n Do you want to save?",
+    first_enter_password : "You must first enter the password...",
+    text_lost : "If you continue, the current text will be lost.",
+    apply_new_work_factor : "Note: To apply the new work factor to an existing text, \nyou must change the password or the title.",
+	// psw_title_action.js
+	 enter_text_placeholder: "Enter your text...",
+	 wrong_password: "Wrong password",
+	 existing_title: "There is already a content named ",
+	 import_overwrites: "Import will overwrite the existing content...", 
+	 type_and_confirm: "You must type and confirm a password...",
+	 confirm_password: "You must confirm the password...",
+	 password_confirm_not_equal: "Password and confirmed password are not equal",
+	 no_title_error: "Error: There is no title",
+	// file_action.js
+	 missing_required_json_key: "JSON object does not contain required key", 
+	 not_valid_json: "File content is not valid JSON format...", 
+	 invalid_notebook_file: "Invalid file for Browser Notebook: ", 
+	 no_support_download: "Your browser does not support this download function",
+	 no_support_upload: "Your browser does not support file upload...",
+	// page_view.js
+	 no_text_create: "There is no text. \nCreate a new text: \n  File -> New...",
+	 // pell_integration
+	 select_image: "Select an image file"
+};
+
