9296378ef590ff788ebd072e7c19fffb67ae0cff
Axel von dem Bruch <Beloumi@users.noreply.github.com>
2018-02-21 10:49:51 +0100

    Create lang.tr.js
    version 0.4-beta

---
+++ js/lang/lang.tr.js
@@ -0,0 +1,31 @@
+// lang.en.js
+"use strict";
+var lang = {
+	//browser-notebook.js
+	 unexpected_error: "beklenmeyen hata.",
+	 no_text: "hiçbir metin yok",
+    unsaved_changes : "orada kaydedilmemiş değişiklikler..\n kaydetmek istiyor musunuz?",
+    first_enter_password : "önce şifrenizi girmelisiniz...",
+    text_lost : "devam ederseniz, geçerli metin kaybolur.",
+    apply_new_work_factor : "Not: varolan bir metne yeni iş faktörü uygulamak için, \nparolayı veya başlığı değiştir.",
+	// psw_title_action.js
+	 enter_text_placeholder: "metninizi girin...",
+	 wrong_password: "yanlış şifre",
+	 existing_title: "zaten adı olan bir içerik var: ",
+	 import_overwrites: "Ithalat varolan içeriğin üzerine yazacaktır...", 
+	 type_and_confirm: "yazdığınız ve parolayı onaylamanız gerekir...",
+	 confirm_password: "parolayı onaylamanız gerekir...",
+	 password_confirm_not_equal: "parola ve onaylanmış parola eşit değildir",
+	 no_title_error: "hata: hiçbir başlık yok",
+	// file_action.js
+	 missing_required_json_key: "JSON nesne gerekli anahtar içermiyor", 
+	 not_valid_json: "dosya içeriği geçerli JSON biçimi değil...", 
+	 invalid_notebook_file: "tarayıcı defter için geçersiz dosya: ", 
+	 no_support_download: "tarayıcınız bu indirme işlevini desteklemiyor",
+	 no_support_upload: "tarayıcınız dosya yükleme desteklemiyor...",
+	// page_view.js
+	 no_text_create: "hiçbir metin yok. \nyeni bir metin oluşturun: \n  dosya -> yeni...",
+	 // pell_integration
+	 select_image: "Resim dosyası seçin"
+};
+
