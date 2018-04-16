82e2c0f61c37577b25f5dd2c035eff9b89303a5e
Axel von dem Bruch <Beloumi@users.noreply.github.com>
2018-04-16 10:57:47 +0200

    version 0.5

--- js/lang/land.de.js
+++ js/lang/land.de.js
@@ -8,11 +8,12 @@ var lang = {
     first_enter_password : "Zuerst muss das Passwort eingegeben werden...",
     text_lost : "Wenn Du fortfährst, geht der Text verloren...",
     apply_new_work_factor : "Damit der neue Arbeitsfaktor auf den Text angewendet wird, \nmuss das Passwort geändert werden.",
+    memory_limit_exceeded : "Speicherlimit für Browser überschritten",
 	// psw_title_action.js
 	 enter_text_placeholder: "Text eingeben...",
 	 wrong_password: "Falsches Passwort",
 	 existing_title: "Es existiert bereit ein Inhalt mit dem Namen ",
-	 import_overwrites: ".\n Der Import wird den bestehenden Inhalt überschreiben...", 
+	 import_overwrites: "\n Der Import wird den bestehenden Inhalt überschreiben...", 
 	 type_and_confirm: "Passwort eingeben und bestätigen...",
 	 confirm_password: "Passwort bestätigen...",
 	 password_confirm_not_equal: "Passwort und Bestätigung sind nicht gleich",
@@ -26,6 +27,9 @@ var lang = {
 	// page_view.js
 	 no_text_create: "Es liegt kein Text vor. \nErzeuge neuen Text: \n  Datei -> Neu...",
 	 // pell_integration
-	 select_image: "Bilddatei auswählen"
+	 select_image: "Bilddatei auswählen",
+	 invalid_image_file: "Ungültige Bilddatei",
+	 file_size_warning: "Warnung: Große Bilddateien können den verfügbaren Speicher überschreiten.",
+	 // cloud_action.js
+	 file_saved: "Datei gespeichert:"
 };
-
